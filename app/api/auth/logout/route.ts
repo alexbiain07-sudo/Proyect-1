1fcf7085ec80aced2e2b847283ddf43edbca6d9f
martinmunoznz <115111493+martinmunoznz@users.noreply.github.com>
2026-02-27 10:48:22 -0300

    Swap POST and GET methods in logout route

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -1,8 +1,10 @@
 import { NextResponse } from "next/server";
 
 
-export async function POST() {
-  const response = NextResponse.json({ success: true });
+export async function GET(request: Request) {
+  const baseUrl = new URL(request.url).origin;
+
+  const response = NextResponse.redirect(baseUrl);
 
   response.cookies.set("meucci_user", "", {
     path: "/",
@@ -13,8 +15,8 @@ export async function POST() {
 }
 
 
-export async function GET() {
-  const response = NextResponse.redirect("/");
+export async function POST(request: Request) {
+  const response = NextResponse.json({ success: true });
 
   response.cookies.set("meucci_user", "", {
     path: "/",
